eee3efb980101896bfcecfda4caa047272aec25f
Phil Mottershaw <philm127@outlook.com>
2020-11-30 17:58:06 -0600

    added create camp controller

---
+++ AdtonesAdminWebApi/BusinessServices/CreateUpdateCampaignService.cs
@@ -0,0 +1,49 @@
+﻿using AdtonesAdminWebApi.Services;
+using AdtonesAdminWebApi.ViewModels;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdtonesAdminWebApi.BusinessServices
+{
+    public class CreateUpdateCampaignService
+    {
+        private readonly IHttpContextAccessor _httpAccessor;
+        ReturnResult result = new ReturnResult();
+
+
+        public CreateUpdateCampaignService(IHttpContextAccessor httpAccessor)
+        {
+            _httpAccessor = httpAccessor;
+        }
+
+
+        /// <summary>
+        /// Gets initial data to populate the create campaign stepper.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ReturnResult> GetInitialData(int advertiserId = 0)
+        {
+            try
+            {
+                result.body = await _advertDAL.GetAdvertResultSet(id);
+
+            }
+            catch (Exception ex)
+            {
+                var _logging = new ErrorLogging()
+                {
+                    ErrorMessage = ex.Message.ToString(),
+                    StackTrace = ex.StackTrace.ToString(),
+                    PageName = "CreateUpdateCampaignService",
+                    ProcedureName = "GetInitialData"
+                };
+                _logging.LogError();
+                result.result = 0;
+            }
+            return result;
+        }
+    }
+}
---
+++ AdtonesAdminWebApi/DAL/Queries/ClientQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdtonesAdminWebApi.DAL.Queries
+{
+    public static class ClientQuery
+    {
+
+        public static string LoadCountryDataTable => @"SELECT ";
+    }
+}
